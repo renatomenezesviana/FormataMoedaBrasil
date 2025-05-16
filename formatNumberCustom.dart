7d85f2f47ba4096bfb339893978b447e5e176e37
renatomenezesviana <101019270+renatomenezesviana@users.noreply.github.com>
2025-05-16 15:02:45 -0300

    Create formatNumberCustom.dart
    Use Custom Functions to go further when it comes to Number Format.  Here are some examples of what you'll learn in this tutorial and how we can display the number 8965245.658:
    
    ✅ R$ 8,97 milhões, R$ 8,97 milhões de reais, $8.97 million, etc, using NumberFormat.compactLong
    ✅ R$ 8,97 mi, US$ 8.97M, etc, using NumberFormat.compactCurrency
    ✅ R$ 8.965.245,658 and many other ways using NumberFormat.currency

---
+++ formatNumberCustom.dart
@@ -0,0 +1,48 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+String? formatNumberCustom(
+  String? stringNumber,
+  String? locale,
+  String? customPattern,
+  String? symbol,
+  int? decimalDigits,
+) {
+  /// MODIFY CODE ONLY BELOW THIS LINE
+  if (stringNumber == null || stringNumber.isEmpty) {
+    FFAppState().APPStateValorDouble = 0;
+    return '0';
+  }
+  try {
+    //Convert the input from String to Double
+    stringNumber = stringNumber?.replaceAll(',', '.');
+    if (stringNumber != null) {
+      var number = double.parse(stringNumber);
+
+      String formatadoString = NumberFormat.currency(
+              //Parameters
+              locale: locale,
+              customPattern: customPattern,
+              symbol: symbol,
+              decimalDigits: decimalDigits)
+          //Format
+          .format(number);
+      FFAppState().APPStateValorDouble = number;
+      return formatadoString;
+    }
+
+    //If something goes wrong, catch the error
+  } catch (e) {
+    //And return '0' to avoid null
+    FFAppState().APPStateValorDouble = 0;
+    return '0';
+  }
+
+  /// MODIFY CODE ONLY ABOVE THIS LINE
+}
